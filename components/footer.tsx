e371f87c2d995b9f2196a47b6638bc6c55b8653e
Ilias bounabat <bounabatilias2004@gmail.com>
2025-03-09 14:45:06 +0100

    footer maj

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
-import styles from '../styles/Footer.module.css';
+import styles from '../styles/footer.module.css';
 
 const Footer: React.FC = () => {
   return (
